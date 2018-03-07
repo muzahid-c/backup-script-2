cd739bf1b74a6a8fcd011bf3a34cea8360f8b5c4
muzahid-c <muzahid@protonmail.com>
2018-03-07 20:40:48 +0600

    Make the comment more clear

--- backup-script-2.cmd
+++ backup-script-2.cmd
@@ -33,6 +33,7 @@ SET DEST="\\server\d$\dest_location"
 
 REM Trying to connect using user and pass of remote destination
 REM Redirect nul to suppress output as echo off don't work on net use 
+REM Do not use DEST variable in net use command as it may not work
 NET USE "\\server\d$\dest_location" /user:%user% %pass% >nul 
 
 ROBOCOPY %SRC% %DEST% /E /PURGE /MT:20 /R:10 /Log:Log_location\%LOG_FILE%
@@ -40,7 +41,7 @@ ROBOCOPY %SRC% %DEST% /E /PURGE /MT:20 /R:10 /Log:Log_location\%LOG_FILE%
 
 REM Deleting all netork mapping to clear session
 
-REM echo ---------------------Closing----------------------------- >> %LOG_LOCATION%%LOGFILE%
-REM echo Disconnecting all network session... >> %LOG_LOCATION%%LOGFILE%
+
+REM Disconnecting network session for next use
 net use "\\server\d$\dest_location" /delete /YES >nul
-REM echo Done! >> %LOG_LOCATION%%LOGFILE%
+REM Done
